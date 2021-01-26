8a08fc7541ae19213544619bd946ed04de48ae06
TAN KOK KEONG <tankk-wm19@student.tarc.edu.my>
2021-01-26 23:39:23 +0800

    fix header issue

--- web-app-assignment/script/app.js
+++ web-app-assignment/script/app.js
@@ -2,12 +2,50 @@
 var a = $("#page-content").height();
 $(document).on('scroll', function () {
     if ($(this).scrollTop() >= 100) {
-        $("#header-nav").css("position", "");
-        $("#header-nav").css("background-color", "black");
-        $("#header-nav").removeClass("bg-transparent");
+
+        if ($(window).width() < 1000) {
+            $("#header-nav").removeClass("bg-transparent");
+            $("#header-nav").css("position", "");
+        }
+        else {
+            $("#header-nav").css("position", "");
+            $("#header-nav").removeClass("bg-transparent");
+        }
+        
     }
     else {
-        $("#header-nav").css("position", "absolute");
-        $("#header-nav").addClass("bg-transparent");
+
+        if ($(window).width() < 1000) {
+            $("#header-nav").removeClass("bg-transparent");
+            $("#header-nav").css("position", "");
+        }
+        else {
+            $("#header-nav").css("position", "absolute");
+            $("#header-nav").addClass("bg-transparent");
+        }
     }
+});
+
+$(document).ready(function () {
+    if ($(window).width() < 1000) {
+        $("#header-nav").removeClass("bg-transparent");
+    }
+
+    $(window).resize(function () {
+        if ($(window).width() < 1000) {
+            $("#header-nav").removeClass("bg-transparent");
+            $("#header-nav").css("position", "");
+        }
+        else {
+            if ($(this).scrollTop() >= 100) {
+                $("#header-nav").removeClass("bg-transparent");
+                $("#header-nav").css("position", "");
+            }
+            else {
+                $("#header-nav").addClass("bg-transparent");
+                $("#header-nav").css("position", "absolute");
+            }
+           
+        }
+    });
 });
