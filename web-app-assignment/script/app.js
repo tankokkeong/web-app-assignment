078dc81fbb623e7a38dcf7ee87f8e964ad2d2e71
TAN KOK KEONG <tankk-wm19@student.tarc.edu.my>
2021-01-29 22:12:17 +0800

    fix header issue

--- web-app-assignment/script/app.js
+++ web-app-assignment/script/app.js
@@ -1,51 +1,58 @@
 ﻿//Script for navbar
-var a = $("#page-content").height();
-$(document).on('scroll', function () {
-    if ($(this).scrollTop() >= 100) {
 
-        if ($(window).width() < 1000) {
-            $("#header-nav").removeClass("bg-transparent");
-            $("#header-nav").css("position", "");
-        }
-        else {
-            $("#header-nav").css("position", "");
-            $("#header-nav").removeClass("bg-transparent");
-        }
-        
-    }
-    else {
+if (window.location.pathname === "/home" || window.location.pathname === "/home.aspx") {
+    var a = $("#page-content").height();
+    $(document).on('scroll', function () {
+        if ($(this).scrollTop() >= 100) {
 
-        if ($(window).width() < 1000) {
-            $("#header-nav").removeClass("bg-transparent");
-            $("#header-nav").css("position", "");
-        }
-        else {
-            $("#header-nav").css("position", "absolute");
-            $("#header-nav").addClass("bg-transparent");
-        }
-    }
-});
-
-$(document).ready(function () {
-    if ($(window).width() < 1000) {
-        $("#header-nav").removeClass("bg-transparent");
-    }
-
-    $(window).resize(function () {
-        if ($(window).width() < 1000) {
-            $("#header-nav").removeClass("bg-transparent");
-            $("#header-nav").css("position", "");
-        }
-        else {
-            if ($(this).scrollTop() >= 100) {
+            if ($(window).width() < 1000) {
                 $("#header-nav").removeClass("bg-transparent");
                 $("#header-nav").css("position", "");
             }
             else {
-                $("#header-nav").addClass("bg-transparent");
-                $("#header-nav").css("position", "absolute");
+                $("#header-nav").css("position", "");
+                $("#header-nav").removeClass("bg-transparent");
             }
-           
+
+        }
+        else {
+
+            if ($(window).width() < 1000) {
+                $("#header-nav").removeClass("bg-transparent");
+                $("#header-nav").css("position", "");
+            }
+            else {
+                $("#header-nav").css("position", "absolute");
+                $("#header-nav").addClass("bg-transparent");
+            }
         }
     });
-});
+
+    $(document).ready(function () {
+        if ($(window).width() < 1000) {
+            $("#header-nav").removeClass("bg-transparent");
+        }
+
+        $(window).resize(function () {
+            if ($(window).width() < 1000) {
+                $("#header-nav").removeClass("bg-transparent");
+                $("#header-nav").css("position", "");
+            }
+            else {
+                if ($(this).scrollTop() >= 100) {
+                    $("#header-nav").removeClass("bg-transparent");
+                    $("#header-nav").css("position", "");
+                }
+                else {
+                    $("#header-nav").addClass("bg-transparent");
+                    $("#header-nav").css("position", "absolute");
+                }
+
+            }
+        });
+
+        //Add Class by default
+        $("#header-nav").addClass("bg-transparent");
+        $("#header-nav").css("position", "absolute");
+    });
+}
