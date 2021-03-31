e3e5649976cf47b3ecc08b65d2dcf483bee9c842
anson_hzs01 <zsho.anson11167@gmail.com>
2021-03-31 13:15:34 +0800

    nav link

--- web-app-assignment/Site1.Master.designer.cs
+++ web-app-assignment/Site1.Master.designer.cs
@@ -147,7 +147,7 @@ namespace web_app_assignment
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor logoutLink;
+        protected global::System.Web.UI.WebControls.HyperLink logoutLink;
 
         /// <summary>
         /// form1 control.
