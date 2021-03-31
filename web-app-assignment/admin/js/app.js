80953406a3827712f3c5bf604b32f072142d0b88
TAN KOK KEONG <tankk-wm19@student.tarc.edu.my>
2021-03-31 22:28:57 +0800

    admin client management page

--- web-app-assignment/admin/js/app.js
+++ web-app-assignment/admin/js/app.js
@@ -1,3 +1,18 @@
 ﻿function navbarActive() {
     nav_option = document.getElementsByClassName("app-menu__item");
 }
+
+function changeClientsType() {
+    selected_option = document.getElementById("client-management-option").value;
+
+    if (selected_option == "Job Seeker") {
+
+        //Display Job Seeker management
+        window.location.href = "clients-management.aspx";
+    }
+    else {
+
+        //Display Recruiter management
+        window.location.href = "clients-management-r.aspx";
+    }
+}
